fb1a7f605ff3b0751ee96c1d06ce72c9e339d544
nobelium24 <ogunbaja07@gmail.com>
2025-06-16 19:34:16 +0100

    minor update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <header className="bg-gray-800 text-white shadow-sm">
             <div className="max-w-6xl mx-auto px-4 py-6 flex justify-between items-center">
-                <h1 className="text-2xl font-bold">Ogunba Joseph Adewole (Fullstack Developer)</h1>
+                <h1 className="text-2xl font-bold">OJA</h1>
                 <nav>
                     <ul className="flex space-x-6">
                         <li><a href="#about" className="text-white hover:text-primary">About</a></li>
